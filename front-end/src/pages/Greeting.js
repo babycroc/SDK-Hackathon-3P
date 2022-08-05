fde30c59716f47f480e4afb2d6467f22e7ac8488
smreosms13 <smreosms13@gamil.com>
2022-08-06 05:29:27 +0900

    change btn color and reset greeting text timing

--- front-end/src/pages/Greeting.js
+++ front-end/src/pages/Greeting.js
@@ -26,7 +26,7 @@ function Greeting () {
         const timer = setInterval(() => {
             setIndex(index + 1);
             // console.log(index)
-        }, 1000);
+        }, 4000);
         console.log(index)
 
         if(index >= greeting_text.length - 1) clearInterval(timer);
